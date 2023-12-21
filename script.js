9684e10bdd6abe4400f238dd0ee251d54d8151ea
Grey Pegram <dpegram85@gmail.com>
2023-12-21 17:01:40 -0500

    Refine EE, hiddenElement

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ h2.textContent = "This content added by JavaScript";
 const gpText = document.getElementById('gp');
 
 const colors = [
-  '#2D00F7',
-  '#6A00F4',
-  '#8900F2',
-  '#A100F2',
+
   '#B100E8',
   '#BC00DD',
   '#D100D1',
   '#DB00B6',
   '#E500A4',
-  '#F20089'
+  '#F20089',
+  '#E500A4',
+  '#DB00B6',
+  '#D100D1',
+  '#BC00DD',
+  '#B100E8'
+  
 ];
 
 let currentIndex = 0;
@@ -24,4 +27,22 @@ let currentIndex = 0;
 gpText.addEventListener('click', function() {
   gpText.style.color = colors[currentIndex];
   currentIndex = (currentIndex + 1) % colors.length;
+});
+
+
+const reloadColor = document.getElementById('gp');
+
+document.addEventListener('keypress', function(event) {
+    if (event.key === "r") {
+        reloadColor.style.color = '#e5e5e5'; /* reset the color to original value */
+    }
+});
+
+const showElementLink = document.getElementById('showElementLink');
+const hiddenElement = document.getElementById('hiddenElement');
+
+showElementLink.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the link from navigating
+  
+  hiddenElement.classList.add('shown'); /* Show the hidden element */
 });
